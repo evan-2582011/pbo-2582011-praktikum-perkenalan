5e450667891a7be3ce04eb541cc05f6bdbc7fb38
evan-2582011 <2582011@unai.edu>
2026-09-07 14:01:03 +0700

    menambahkan program perkenalan

--- src/perkenalan.java
+++ src/perkenalan.java
@@ -14,6 +14,17 @@ public class perkenalan {
         int umur = input.nextInt();
         input.nextLine();
 
+        System.out.print("Masukkan tinggi: ");
+        double tinggi = input.nextDouble();
+        input.nextLine(); // buang sisa enter
+
+        System.out.print("Masukkan asal kota: ");
+        String asalKota = input.nextLine();
+
+        System.out.println();
+        System.out.println("Halo! Nama saya " + nama + " (NIM " + nim + "), umur "
+                + umur + " tahun, tinggi " + tinggi + " cm, berasal dari " + asalKota + ".");
+
 
     }
 }
