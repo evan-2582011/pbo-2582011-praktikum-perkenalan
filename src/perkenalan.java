1ce87307ede45c015820a03cac27eddedad68a1d
evan-2582011 <2582011@unai.edu>
2026-09-07 13:53:47 +0700

    menambahkan program perkenalan

--- src/perkenalan.java
+++ src/perkenalan.java
@@ -4,25 +4,5 @@ public class perkenalan {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Masukkan nama\t\t: ");
+        System.out.print("Masukkan nama: ");
         String nama = input.nextLine();
-
-        System.out.print("Masukkan NIM\t\t: ");
-        int nim = input.nextInt();
-
-        System.out.print("Masukkan umur\t\t: ");
-        int umur = input.nextInt();
-        input.nextLine(); // buang sisa enter
-
-        System.out.print("Masukkan tinggi\t\t: ");
-        double tinggi = input.nextDouble();
-        input.nextLine(); // buang sisa enter
-
-        System.out.print("Masukkan asal kota\t: ");
-        String asalKota = input.nextLine();
-
-        System.out.println();
-        System.out.println("Halo! Nama saya " + nama + " (NIM " + nim + "), umur "
-                + umur + " tahun, tinggi " + tinggi + " cm, berasal dari " + asalKota + ".");
-    }
-}
